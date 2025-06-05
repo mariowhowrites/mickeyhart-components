9f802cfec5eab318f337cf437b87cb0a8a225106
Mario Vega <mariovegadev@gmail.com>
2025-06-04 17:54:47 -0700

    try adding cmp to gh pages

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ import replace from '@rollup/plugin-replace';
 export default {
   input: 'my-element.js',
   output: {
-    file: 'my-element.bundled.js',
+    file: 'docs/my-element.bundled.js',
     format: 'esm',
     exports: 'named'
   },
